faa88bb3879be1a09a61118929e299d5d3e606ca
guythiebaut <guythiebaut@gmail.com>
2011-11-06 12:00:28 +0000

    TeboCam.sln added

--- copy_commands/copy_to_source_control.bat
+++ copy_commands/copy_to_source_control.bat
@@ -1,3 +1,4 @@
+copy "F:\Visual Studio 2005\Projects\TeboCam\TeboCam.sln" "F:\Visual Studio 2005\Projects\TeboCam_OpenSource\TeboCam.sln"
 copy "F:\Visual Studio 2005\Projects\TeboCam\TeboCam\AssemblyInfo.cs" "F:\Visual Studio 2005\Projects\TeboCam_OpenSource\source_code\AssemblyInfo.cs"
 copy "F:\Visual Studio 2005\Projects\TeboCam\TeboCam\bubble.cs" "F:\Visual Studio 2005\Projects\TeboCam_OpenSource\source_code\bubble.cs"
 copy "F:\Visual Studio 2005\Projects\TeboCam\TeboCam\calibrate.cs" "F:\Visual Studio 2005\Projects\TeboCam_OpenSource\source_code\calibrate.cs"
